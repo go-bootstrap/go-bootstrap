8caf328217956aef0658308a474d3adec15c935f
Didip Kerabat <didipk@gmail.com>
2016-01-02 17:00:23 -0800

    Put application in its own package and use github.com/spf13/viper as configuration struct.

--- blank/main.go
+++ blank/main.go
@@ -3,107 +3,65 @@ package main
 import (
 	"encoding/gob"
 	"fmt"
-	"$GO_BOOTSTRAP_REPO_NAME/$GO_BOOTSTRAP_REPO_USER/$GO_BOOTSTRAP_PROJECT_NAME/dal"
-	"$GO_BOOTSTRAP_REPO_NAME/$GO_BOOTSTRAP_REPO_USER/$GO_BOOTSTRAP_PROJECT_NAME/handlers"
-	"$GO_BOOTSTRAP_REPO_NAME/$GO_BOOTSTRAP_REPO_USER/$GO_BOOTSTRAP_PROJECT_NAME/libenv"
-	"$GO_BOOTSTRAP_REPO_NAME/$GO_BOOTSTRAP_REPO_USER/$GO_BOOTSTRAP_PROJECT_NAME/libunix"
-	"$GO_BOOTSTRAP_REPO_NAME/$GO_BOOTSTRAP_REPO_USER/$GO_BOOTSTRAP_PROJECT_NAME/middlewares"
-	"github.com/carbocation/interpose"
-	gorilla_mux "github.com/gorilla/mux"
-	"github.com/gorilla/sessions"
-	"github.com/jmoiron/sqlx"
-	_ "github.com/lib/pq"
 	"github.com/Sirupsen/logrus"
+	"github.com/spf13/viper"
 	"github.com/tylerb/graceful"
 	"net/http"
 	"time"
+
+	"$GO_BOOTSTRAP_REPO_NAME/$GO_BOOTSTRAP_REPO_USER/$GO_BOOTSTRAP_PROJECT_NAME/application"
+	"$GO_BOOTSTRAP_REPO_NAME/$GO_BOOTSTRAP_REPO_USER/$GO_BOOTSTRAP_PROJECT_NAME/dal"
+	"$GO_BOOTSTRAP_REPO_NAME/$GO_BOOTSTRAP_REPO_USER/$GO_BOOTSTRAP_PROJECT_NAME/libunix"
 )
 
 func init() {
 	gob.Register(&dal.UserRow{})
 }
 
-// NewApplication is the constructor for Application struct.
-func NewApplication() (*Application, error) {
+func newConfig() (*viper.Viper, error) {
 	u, err := libunix.CurrentUser()
 	if err != nil {
 		return nil, err
 	}
 
-	dsn := libenv.EnvWithDefault("DSN", fmt.Sprintf("postgres://%v@localhost:5432/$GO_BOOTSTRAP_PROJECT_NAME?sslmode=disable", u))
+	c := viper.New()
+	c.SetDefault("dsn", fmt.Sprintf("postgres://%v@localhost:5432/$GO_BOOTSTRAP_PROJECT_NAME?sslmode=disable", u))
+	c.SetDefault("cookie_secret", "$GO_BOOTSTRAP_COOKIE_SECRET")
+	c.SetDefault("http_addr", ":8888")
+	c.SetDefault("http_cert_file", "")
+	c.SetDefault("http_key_file", "")
+	c.SetDefault("http_drain_interval", "1s")
 
-	db, err := sqlx.Connect("postgres", dsn)
-	if err != nil {
-		return nil, err
-	}
+	c.AutomaticEnv()
 
-	cookieStoreSecret := libenv.EnvWithDefault("COOKIE_SECRET", "$GO_BOOTSTRAP_COOKIE_SECRET")
-
-	app := &Application{}
-	app.dsn = dsn
-	app.db = db
-	app.cookieStore = sessions.NewCookieStore([]byte(cookieStoreSecret))
-
-	return app, err
-}
-
-// Application is the application object that runs HTTP server.
-type Application struct {
-	dsn         string
-	db          *sqlx.DB
-	cookieStore *sessions.CookieStore
-}
-
-func (app *Application) middlewareStruct() (*interpose.Middleware, error) {
-	middle := interpose.New()
-	middle.Use(middlewares.SetDB(app.db))
-	middle.Use(middlewares.SetCookieStore(app.cookieStore))
-
-	middle.UseHandler(app.mux())
-
-	return middle, nil
-}
-
-func (app *Application) mux() *gorilla_mux.Router {
-	MustLogin := middlewares.MustLogin
-
-	router := gorilla_mux.NewRouter()
-
-	router.Handle("/", MustLogin(http.HandlerFunc(handlers.GetHome))).Methods("GET")
-
-	router.HandleFunc("/signup", handlers.GetSignup).Methods("GET")
-	router.HandleFunc("/signup", handlers.PostSignup).Methods("POST")
-	router.HandleFunc("/login", handlers.GetLogin).Methods("GET")
-	router.HandleFunc("/login", handlers.PostLogin).Methods("POST")
-	router.HandleFunc("/logout", handlers.GetLogout).Methods("GET")
-
-	router.Handle("/users/{id:[0-9]+}", MustLogin(http.HandlerFunc(handlers.PostPutDeleteUsersID))).Methods("POST", "PUT", "DELETE")
-
-	// Path of static files must be last!
-	router.PathPrefix("/").Handler(http.FileServer(http.Dir("static")))
-
-	return router
+	return c, nil
 }
 
 func main() {
-	app, err := NewApplication()
+	config, err := newConfig()
 	if err != nil {
-		logrus.Fatal(err.Error())
+		logrus.Fatal(err)
 	}
 
-	middle, err := app.middlewareStruct()
+	app, err := application.New(config)
 	if err != nil {
-		logrus.Fatal(err.Error())
+		logrus.Fatal(err)
 	}
 
-	serverAddress := libenv.EnvWithDefault("HTTP_ADDR", ":8888")
-	certFile := libenv.EnvWithDefault("HTTP_CERT_FILE", "")
-	keyFile := libenv.EnvWithDefault("HTTP_KEY_FILE", "")
-	drainIntervalString := libenv.EnvWithDefault("HTTP_DRAIN_INTERVAL", "1s")
+	middle, err := app.MiddlewareStruct()
+	if err != nil {
+		logrus.Fatal(err)
+	}
+
+	serverAddress := config.Get("http_addr").(string)
+
+	certFile := config.Get("http_cert_file").(string)
+	keyFile := config.Get("http_key_file").(string)
+	drainIntervalString := config.Get("http_drain_interval").(string)
 
 	drainInterval, err := time.ParseDuration(drainIntervalString)
 	if err != nil {
-		logrus.Fatal(err.Error())
+		logrus.Fatal(err)
 	}
 
 	srv := &graceful.Server{
@@ -111,13 +69,15 @@ func main() {
 		Server:  &http.Server{Addr: serverAddress, Handler: middle},
 	}
 
-	logrus.Infoln("Running HTTP server on "+ serverAddress)
+	logrus.Infoln("Running HTTP server on " + serverAddress)
+
 	if certFile != "" && keyFile != "" {
 		err = srv.ListenAndServeTLS(certFile, keyFile)
 	} else {
 		err = srv.ListenAndServe()
 	}
+
 	if err != nil {
-		logrus.Fatal(err.Error())
+		logrus.Fatal(err)
 	}
 }
